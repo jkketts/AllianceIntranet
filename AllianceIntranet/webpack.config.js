3d367f61c3be957079b05fb235486e234311ea91
Justin Kettemran <justin@ketterman.tv>
2018-06-27 11:05:18 -0500

    Added React

---
+++ AllianceIntranet/webpack.config.js
@@ -0,0 +1,20 @@
+﻿const path = require('path');
+
+module.exports = {
+    entry: './wwwroot/src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'wwwroot/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+};
---
+++ AllianceIntranet/wwwroot/src/index.js
@@ -0,0 +1,8 @@
+﻿import React from "react";
+import ReactDOM from "react-dom";
+
+const Index = () => {
+    return <div>react is running in my app.. without node this is real?</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
